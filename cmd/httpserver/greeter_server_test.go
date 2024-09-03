8714a87b901a4e89e5a050b8df01d66a9126c657
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-03 09:25:02 +0500

    feat: added test, driver, docker

--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -1,11 +1,38 @@
 package main_test
 
 import (
+	"context"
 	"testing"
 
+	go_specs_greet "github.com/ArsaGit/go-specs-greet"
 	"github.com/ArsaGit/go-specs-greet/specifications"
+	"github.com/alecthomas/assert/v2"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	specifications.GreetSpecification(t, nil)
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:    "../../.",
+			Dockerfile: "./cmd/httpserver/Dockerfile",
+			// set to false if you want less spam, but this is helpful if you're having troubles
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
+	specifications.GreetSpecification(t, driver)
 }
